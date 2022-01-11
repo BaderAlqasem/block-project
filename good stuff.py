4afea11abd014a3a027c6d340affb9da025b0b5b
BaderAlqasem <90061531+BaderAlqasem@users.noreply.github.com>
2022-01-11 10:32:39 -0700

    Update good stuff.py

--- good stuff.py
+++ good stuff.py
@@ -1,8 +1,9 @@
 import pygame
 import random
-from threading import Timer
 import sys
-# import pygame_textinput
+
+score = 0
+
 
 def button():
     pygame.init()
@@ -12,7 +13,6 @@ def button():
     screen_height = 600
     screen = pygame.display.set_mode([screen_width, screen_height])
     
-
     WHITE = (255,255,255)
     GREY = (128, 128, 128)
 
@@ -35,12 +35,16 @@ def button():
     heightEat = 700
     
     smallfont = pygame.font.SysFont('Corbel',35)
-    
 
     quitBtn = smallfont.render("Quit" , True , WHITE)
     avoidBtn = smallfont.render("Avoid" , True , WHITE)
     eatBtn = smallfont.render("Eat" , True , WHITE)
     
+
+
+
+
+
     while True:
 
         for event in pygame.event.get():
@@ -80,8 +84,8 @@ def button():
 
         pygame.display.update()
 
-
 def eatBlocks():
+    name = input("enter your name")
 
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
@@ -102,20 +106,16 @@ def eatBlocks():
     # class for all blocks in the game
     class Block(pygame.sprite.Sprite):
     
-        def __init__(self, WHITE, width, height):
+        def __init__(self, color, width, height):
         
             # Call the parent class (Sprite) constructor
             super().__init__()
 
-            # Create an image of the block, and fill it with a WHITE.
+            # Create an image of the block, and fill it with a color.
             # This could also be an image loaded from the disk.
             self.image = pygame.Surface([width, height])
-            self.image.fill(WHITE)
+            self.image.fill(color)
 
-            # Fetch the rectangle object that has the dimensions of the image
-            # image.
-            # Update the position of this object by setting the values
-            # of rect.x and rect.y
             self.rect = self.image.get_rect()
 
     pygame.init()
@@ -139,6 +139,7 @@ def eatBlocks():
         # Add the block to the list of objects
         block_list.add(food)
         all_sprites_list.add(food)
+
     # green player 
     player = Block(RED, 20, 20)
     all_sprites_list.add(player)
@@ -177,6 +178,8 @@ def eatBlocks():
                 elif event.key == pygame.K_s:
                     y1_change = 5
                     x1_change = 0
+    
+
         # Positioning of player block updates after key is pressed
         x1 += x1_change
         y1 += y1_change
@@ -190,29 +193,59 @@ def eatBlocks():
         # player block positioning which updates after x1, y1 change is applied
         player.rect.x = x1
         player.rect.y = y1
+
         # See if the player block has collided with anything.
         blocks_hit_list = pygame.sprite.spritecollide(player, block_list, True)
         # Check the list of collisions.
         for block in blocks_hit_list:
             score +=1
             print(score)
+
+    #High Score Stuff
+    file = open("highscore.txt", "a")
+    file.write(str(score) + ", " + name + "\n")
+    file.close()
+
+    file = open("highscore.txt", "r")
+    readthefile = file.readlines()
+    sortedData = sorted(readthefile, reverse = True)
+
+    print("Top 3 Scores!")
+    print("Pos\tPoints , Name")
+
+    for line in range(3):
+        print(str(line + 1) + "\t" + str(sortedData[line]))
+
+    # if score < 10:
+    #     score = str(score)
+    #     score = score.zfill(2)
+    # else:
+    #     pass
+
+        
         # Check for win
         if score == 50:
             print("You won!")
             loop = False
+
+            
+
+
+
         # Draw all the spites
         all_sprites_list.draw(screen)
         # Score stuff                             bold #Italic
         font = pygame.font.SysFont('Calibri', 25, True, False)
         text = font.render("Score: " + str(score), True, WHITE)
         screen.blit(text, [350, 10])
+
         #Update the screen with drawing.
         pygame.display.flip()
         # 60 FPS
         clock.tick(60)
     print("quit")
     pygame.quit()
-
+    
 def avoidBlocks():
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
@@ -234,11 +267,21 @@ def avoidBlocks():
     # Variables
     score = 0
 
+
+
+    # name = input("enter your name")
+    # file = open("highscore.txt", "a")
+    # file.write(str(score) + "," + name + "\n")
+    # file.close()
+
+
+
+
     # class for all blocks in the game
     class Block(pygame.sprite.Sprite):
-    
+
         def __init__(self, WHITE, width, height):
-        
+
             # Call the parent class (Sprite) constructor
             super().__init__()
 
@@ -261,13 +304,10 @@ def avoidBlocks():
 
     screen = pygame.display.set_mode([screen_width, screen_height])
 
-
     block_list_avoid = pygame.sprite.Group()
-    block_list_collectCoins = pygame.sprite.Group()
-
+    block_list_collectcoin = pygame.sprite.Group()
 
     all_sprites_list = pygame.sprite.Group()
-
 
     player = Block(RED, blockDimensionX, blockDimensionY)
     all_sprites_list.add(player)
@@ -276,6 +316,7 @@ def avoidBlocks():
 
     clock = pygame.time.Clock()
 
+    # Create obstacles
     for i in range(50):
 
         obstacle = Block(BLUE, 20, 15)
@@ -285,7 +326,12 @@ def avoidBlocks():
 
         block_list_avoid.add(obstacle)
         all_sprites_list.add(obstacle)
-   
+
+    # Create a coin
+    coin = Block(YELLOW, 20, 15)
+    coin.rect.x = 100
+    coin.rect.y = 100
+
     # -------- Main Program Loop -----------
     while loop:
         for event in pygame.event.get(): 
@@ -293,17 +339,30 @@ def avoidBlocks():
                 loop = False
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a and score >= 3:
-                    x1_change = -3
+                if event.key == pygame.K_a and score >= 5:
+                    x1_change = -5
                     y1_change = 0
-                elif event.key == pygame.K_d and score >= 3:
-                    x1_change = 3
+                elif event.key == pygame.K_d and score >= 5:
+                    x1_change = 5
                     y1_change = 0
-                elif event.key == pygame.K_w and score >= 3:
-                    y1_change = -3
+                elif event.key == pygame.K_w and score >= 5:
+                    y1_change = -5
                     x1_change = 0
-                elif event.key == pygame.K_s and score >= 3:
-                    y1_change = 3
+                elif event.key == pygame.K_s and score >= 5:
+                    y1_change = 5
+                    x1_change = 0
+
+                elif event.key == pygame.K_a and score >= 10:
+                    x1_change = -7
+                    y1_change = 0
+                elif event.key == pygame.K_d and score >= 10:
+                    x1_change = 7
+                    y1_change = 0
+                elif event.key == pygame.K_w and score >= 10:
+                    y1_change = -7
+                    x1_change = 0
+                elif event.key == pygame.K_s and score >= 10:
+                    y1_change = 7
                     x1_change = 0
 
                 elif event.key == pygame.K_a:
@@ -322,16 +381,11 @@ def avoidBlocks():
         x1 += x1_change
         y1 += y1_change 
 
-        # If player block hits walls, game over
         if x1 >= screen_width or x1 < 0 or y1 >= screen_height or y1 < 0:
             loop = False
             print ("Game over")
-
         # Clear the screen
         screen.fill(BLACK)
-
-        coins = Block(YELLOW, 20, 15)
-
 
         # player block positioning which updates after x1, y1 change is applied
         player.rect.x = x1
@@ -339,26 +393,40 @@ def avoidBlocks():
 
         # See if the player block has collided with anything.
         blocks_hit_list_avoid = pygame.sprite.spritecollide(player, block_list_avoid, True)
-        block_hit_list_collectCoins = pygame.sprite.spritecollide(player, block_list_collectCoins, True)
+        block_hit_list_collectcoin = pygame.sprite.spritecollide(player, block_list_collectcoin, True)
 
-        # for i in range(1):
-        for block in block_hit_list_collectCoins:
-            if block_hit_list_collectCoins:
+        for block in block_hit_list_collectcoin:
+            if block_hit_list_collectcoin:
                 score += 1
-                coins.rect.x = random.randrange(screen_width)
-                coins.rect.y = random.randrange(screen_height)
+                coin.rect.x = random.randrange(screen_width) 
+                coin.rect.y = random.randrange(screen_height) 
 
         # Add the block to the list of objects
-        block_list_collectCoins.add(coins)
-        all_sprites_list.add(coins)
+        block_list_collectcoin.add(coin)
+        all_sprites_list.add(coin)
 
+    name = input("What is your name? ")
+    file = open("highscore.txt", "a")
+    file.write(str(score) + "," + name + "\n")
+    file.close()
+
+    file = open("highscore.txt", "r")
+    readthefile = file.readlines()
+    sortedData = sorted(readthefile, reverse = True)
+
+    print("Top 3 Scores!")
+    print("Prs\tPoints , Name")
+    for line in range(3):
+        print(str(line + 1) + "\t" + str(sortedData[line]))
 
         # Check the list of collisions
-        
+
         for block in blocks_hit_list_avoid:
             print("Game Over")
+            print("Top 3 Scores!")
+            print("Prs\tPoints , Name")
+            # print(d)
             pygame.quit() 
-            
 
         # Check for win
         if score == 50:
